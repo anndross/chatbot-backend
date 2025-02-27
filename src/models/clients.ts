3baebe653f48d6b8265553fbfafe9b90e88e20af
Daycross <dev.sapucaia@gmail.com>
2025-02-27 15:38:42 -0300

    chore: remove debbuging code

--- src/models/clients.ts
+++ src/models/clients.ts
@@ -9,49 +9,24 @@ export interface Client {
   services: string[];
 }
 
-// Simulated list of allowed client hosts
-const allowedClients = [
-    {
-      clientId: "khelf_cb",
-      name: "khelf",
-      hostname: "www.khelf.com.br",
-      paymentStatus: "paid",
-      services: [
-        "chatbot", 
-        "vtex"
-      ],
-    },
-    {
-      clientId: "casa_mais_facil_cb",
-      name: "casa_mais_facil",
-      hostname: "www.casamaisfacil.com.br",
-      paymentStatus: "paid",
-      services: [
-        "chatbot", 
-        "vtex"
-      ],
-    },
-  ];
 
+// Função para obter os dados de um cliente a partir do clientId
+const getClient = async (clientId: string): Promise<Client | null> => {
+  const querySnapshot = await db
+    .collection('users')
+    .where('clientId', '==', clientId)
+    .get();
   
-  // Função para obter os dados de um cliente a partir do clientId
-  const getClient = async (clientId: string): Promise<Client | null> => {
-    const querySnapshot = await db
-      .collection('users')
-      .where('clientId', '==', clientId)
-      .get();
-    
-    if (!querySnapshot.empty) {
-      // Retorna o primeiro documento encontrado, convertendo os dados para o tipo Client
-      const clientData = querySnapshot.docs[0].data() as Client;
-      console.log('🔍 Cliente encontrado:', clientData);
+  if (!querySnapshot.empty) {
+    // Retorna o primeiro documento encontrado, convertendo os dados para o tipo Client
+    const clientData = querySnapshot.docs[0].data() as Client;
 
-      return clientData;
-    }
-  
-    return null;
-  };
-  
+    return clientData;
+  }
+
+  return null;
+};
+
 
 /**
  * Check if the clientId (host) is valid.
@@ -61,7 +36,7 @@ export const isValidClient = async  (clientId: string): Promise<boolean> => {
   const client = await getClient(clientId);
 
   if (client && client.paymentStatus === 'paid') {
-    console.log('✅ Cliente pagante:', client);
+    console.log('✅ Cliente pagante:');
     return true;
   }
   console.log('❌ Cliente não pagante:', client);
