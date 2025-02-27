43b9765a5c51bb22a1c9002ed1e7f215258dbfe4
Daycross <dev.sapucaia@gmail.com>
2025-02-27 17:17:58 -0300

    fix: fix allowed cors parse

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,7 +3,9 @@ import cors from 'cors';
 require('dotenv').config();
 
 // Obtendo as origens permitidas de uma variável de ambiente
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || ['http://127.0.0.1:5500'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim())
+  : ['http://127.0.0.1:5500'];
 
 export const corsMiddleware = cors({
   origin: (origin, callback) => {
